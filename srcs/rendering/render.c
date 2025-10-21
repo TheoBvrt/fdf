30cc6cbc102ae4d79d119617e0cd792bb8aad887
Theo Bouverat <thbouver@c2r3s5.42lausanne.ch>
2025-10-21 17:57:54 +0200

    update

--- srcs/rendering/render.c
+++ srcs/rendering/render.c
@@ -6,22 +6,11 @@
 /*   By: thbouver <thbouver@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 12:03:34 by thbouver          #+#    #+#             */
-/*   Updated: 2025/10/21 17:32:46 by thbouver         ###   ########.fr       */
+/*   Updated: 2025/10/21 17:57:48 by thbouver         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "fdf.h"
-
-void    dda_line(t_fdf *fdf, t_pos start_pos, t_pos end_pos)
-{
-	int	draw_x;
-	int	draw_y;
-	int	steps;
-
-	draw_x = start_pos.x - end_pos.x;
-	draw_x = start_pos.y - end_pos.y;
-	
-}
 
 void fdf_rendering(t_fdf *fdf)
 {
