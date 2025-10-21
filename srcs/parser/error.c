b8cdf487e5a1ab600feaadc8da5853959c1fefff
Theo Bouverat <thbouver@c2r3s5.42lausanne.ch>
2025-10-21 15:44:33 +0200

    update

--- srcs/parser/error.c
+++ srcs/parser/error.c
@@ -6,27 +6,27 @@
 /*   By: thbouver <thbouver@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 12:03:24 by thbouver          #+#    #+#             */
-/*   Updated: 2025/10/21 14:47:54 by thbouver         ###   ########.fr       */
+/*   Updated: 2025/10/21 15:36:10 by thbouver         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-int extension_checker(char *file_name, char *extension)
+int	extension_checker(char *file_name, char *extension)
 {
-	int index;
+	int	index;
 
 	index = ft_strlen(file_name) - ft_strlen(extension);
 	if (index < 0)
 	{
-		ft_printf("[erreur] : merci de selectionner un fichier .fdf");
+		ft_putstr_fd("[erreur] : merci de selectionner un fichier .fdf", 0);
 		return (1);
 	}
 	while (file_name[index] && *extension)
 	{
 		if (file_name[index] != *extension)
 		{
-			ft_printf("[erreur] : merci de selectionner un fichier .fdf");
+			ft_putstr_fd("[erreur] : merci de selectionner un fichier .fdf", 0);
 			return (1);
 		}
 		index ++;
@@ -41,17 +41,17 @@ int	args_checker(int argc, char **argv)
 
 	if (argc != 2)
 	{
-		ft_printf("[erreur] : arguments invalide ./fdf <file>\n");
+		ft_putstr_fd("[erreur] : arguments invalide ./fdf <file>\n", 0);
 		return (1);
 	}
 	file = open(argv[1], O_RDONLY);
 	if (file == -1)
 	{
-		ft_printf("[erreur] : impossible d'ouvrir le fichier\n");
+		ft_putstr_fd("[erreur] : impossible d'ouvrir le fichier\n", 0);
 		return (1);
 	}
 	close (file);
-	if (!extension_checker(argv[1], ".fdf"))	
+	if (!extension_checker(argv[1], ".fdf"))
 		return (1);
-    return (0);
+	return (0);
 }
--- srcs/parser/map_parser.c
+++ srcs/parser/map_parser.c
@@ -6,7 +6,7 @@
 /*   By: thbouver <thbouver@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 12:03:29 by thbouver          #+#    #+#             */
-/*   Updated: 2025/10/21 15:10:15 by thbouver         ###   ########.fr       */
+/*   Updated: 2025/10/21 15:43:55 by thbouver         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,28 @@
 
 int	check_tab(char *tmp, t_fdf *fdf)
 {
+	int	check_len;
 	int	y;
 	int	x;
 
 	y = 0;
 	fdf->_heightmap = ft_split(tmp, '\n');
+	check_len = ft_strlen(fdf->_heightmap[0]);
 	while (fdf->_heightmap[y])
 	{
 		x = 0;
+		if (ft_strlen(fdf->_heightmap[y]) != check_len)
+		{
+			free_tab(fdf->_heightmap);
+			ft_putstr_fd("[erreur] : la map doit etre rectangulaire", 0);
+			return (1);
+		}
 		while (fdf->_heightmap[y][x])
 		{
 			if (!ft_isdigit(fdf->_heightmap[y][x]))
 			{
 				free_tab(fdf->_heightmap);
+				ft_putstr_fd("[erreur] : une map ne peux pas contenir autre choses que des chiffres", 0);
 				return (1);
 			}
 			x ++;
@@ -34,14 +43,12 @@ int	check_tab(char *tmp, t_fdf *fdf)
 		y ++;
 	}
 	free_tab(fdf->_heightmap);
-	
 }
 
 int	parse_map(char *file_name, t_fdf *fdf)
 {
 	char	*line;
 	char	*tmp;
-	char	**char_tab;
 	int		fd;
 
 	tmp = ft_calloc(1, sizeof(char));
